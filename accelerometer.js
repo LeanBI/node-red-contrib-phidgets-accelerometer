31bf66390f34595e7df9960ac32b761697c33412
sbrennion <sebastien.brennion@leanbi.ch>
2016-08-04 16:00:24 +0200

    first working

--- accelerometer.js
+++ accelerometer.js
@@ -1,5 +1,6 @@
 module.exports = function(RED) {
     function AccelerometerNode(config) {
+        this.status({fill:"red",shape:"dot",text:"not started"});
         var spawn = require('child_process').spawn;
 
 
@@ -9,35 +10,36 @@ module.exports = function(RED) {
         toExecute=""
         toExecute+=__dirname + "/accelerometer.py";
         node.log(toExecute);
-        //const ls = spawn('python',["/home/leanbi/.node-red/nodes/node-phidgets-accelerometer/accelerometer.py","--message_update_rate", 10 ]);
-        //const ls = spawn('python',["/home/leanbi/.node-red/nodes/node-phidgets-accelerometer/accelerometer.py"]);
-        args=[toExecute,"--sensor_update_rate","3"]
-        node.log("array=" + args);
 
-        const ls = spawn('python',args.concat(toExecute, "test"));
+
+        const ls = spawn('python',["-u", toExecute,"--sensor_update_rate", "3"]);
 
         ls.stdout.on('data', (data) => {
-          node.log(`stdout: ${data}`);
-        });
+            msg=JSON.parse(data)
+            this.status({fill:"green",shape:"dot",text:"receiving data"});
+            //node.log(this.status)
+            node.send(msg);
+          });
 
-        ls.stderr.on('data', (data) => {
-          msg=JSON.parse(data)
-          if (msg.result=="ok") {
-                //node.log("data recieved : " + data)
-                node.send(msg)
-          } else {
-                node.warn("result=" + msg["result"]  + " data=" + data)
-                node.send(msg)
+
+          ls.stderr.on('data', (data) => {
+          msg={ "result":"nok", "payload": data}
+          node.warn("result=" + msg["result"]  + " data=" + data)
+          node.send(msg)
           }
 
-        });
+        );
 
         ls.on('close', (code) => {
-          node.log(`child process exited with code ${code}`);
+            msg=`process exited ${code}`
+          this.status({fill:"red",shape:"dot",text:msg});
+          node.log(msg);
         });
 
         ls.on('error', (err) => {
-            node.log('Failed to start child process.');
+            msg('Failed to start')
+            node.warn(msg);
+            this.status({fill:"red",shape:"dot",text:msg});
         });
 
 
--- accelerometer.py
+++ accelerometer.py
@@ -2,38 +2,99 @@ from __future__ import print_function
 from time import sleep, time
 import random
 import json
+import logging
 
 import argparse
 import sys
+import os
 
 
-parser = argparse.ArgumentParser(description='read data from phidget and print it grouped on stdout')
 
-parser.add_argument('-s', '--sensor_update_rate',action="store_true", help="set the update rate from the sensor (in ms)", default=10 )
-parser.add_argument('-m', '--message_update_rate',action="store_true", help="indicate how long data is being collected before sending a message (in s)", default=10)
-args = parser.parse_args()
+class message:
+    def __init__(self,topic,update_rate=10):
+        self.topic=topic
+        self.time_start=time()
+        self.time_max=self.time_start + update_rate
 
+        self.payload=[]
 
-while True:
-    try :
-        sensor="accelerometer"
+    def add_record(self,myValues):
+        self.payload.append(myValues)
 
-        payload={}
-        payload["tstamp"]=time()
-        payload[sensor + "_" + "x"]=random.random()
-        payload[sensor + "_" + "y"] = random.random()
-        payload[sensor + "_" + "z"] = random.random()
+    def get_json(self,pprint=False):
+        if pprint==False :
+            r=json.dumps(self, default=lambda o: o.__dict__)
+        else :
+            r=json.dumps(self, sort_keys=False, indent=4, default=lambda o: o.__dict__)
+        return r
 
-        response = {"result": "ok", "payload" : payload}
-
-        response_json=json.dumps(response)
-
-        print(response_json,file=sys.stdout)
+    def pprint(self):
+        m=self.get_json(True)
+        print(m, file=sys.stdout)
         sys.stdout.flush()
+        return m
 
-    except Exception as e :
-        msg={ "result" : "nok", "payload" : repr(e)}
-        print(response_json, file=sys.stderr)
+    def nprint(self):
+        m=json.dumps(self,sort_keys=False, indent=4, default=lambda o: o.__dict__ )
+        print(m, file=sys.stdout)
+        sys.stdout.flush()
+        return m
+
+    def overdue(self):
+        now=time()
+        if now > self.time_max :
+            return True
+        else :
+            return False
+
+try :
+
+    #sys.exit(1)
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + '/accelerometer_py.log'
+    logging.basicConfig(filename=dir_path,level=logging.DEBUG)
+    logging.debug(str(sys.argv))
+
+    parser = argparse.ArgumentParser(description='read data from phidget and print it grouped on stdout')
+
+    #parser.add_argument('--sensor_update_rate', nargs=1 ,action="store_true", help="set the update rate from the sensor (in ms)", default=1 )
+    #parser.add_argument('--message_update_rate',action="store_true", help="indicate how long data is being collected before sending a message (in s)", default=10 )
+    parser.add_argument('--sensor_update_rate', nargs=1, type=int )
+    parser.add_argument('--message_update_rate', nargs=1, type=int )
+    args = parser.parse_args()
+
+    logging.debug(args)
 
 
-    sleep(args.message_update_rate)
+    myMessage=None
+    while True:
+        try :
+            if myMessage==None :
+                myMessage=message("accelerometer",10)
+
+            if myMessage.overdue():
+                myMessage = message("accelerometer", 10)
+
+            myMessage.add_record({
+                "tstamp" : time(),
+                "x" : random.random(),
+                "y" : random.random(),
+                "z": random.random()
+            })
+
+            response =  myMessage.pprint()
+            logging.debug("message printed = %s" % response)
+
+        except Exception as e :
+            logging.error("error !!!")
+            logging.exception(e)
+            msg={ "result" : "nok", "payload" : repr(e)}
+
+        logging.debug("sleep for : %s" % args.sensor_update_rate)
+        sleep(float(args.sensor_update_rate[0]))
+
+
+except Exception as e:
+
+    logging.error("pb")
+    logging.exception(e)
+    raise e
